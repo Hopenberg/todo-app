e3f1e191509d21979415ff3bcaea9883a58dacb7
Hopenberg <marcinhopa54@gmail.com>
2022-11-08 13:37:12 +0100

    Repozytorium dla TaskGroup (rozwiazanie problemu n+1 selectow), migracja dla nowych encji

---
+++ src/main/java/io/hopenberg/apps/model/Project.java
@@ -0,0 +1,34 @@
+package io.hopenberg.apps.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "The description cannot be empty")
+    private String description;
+
+    public Project() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
